b39b25cf5408db47793d3158533bd52fa01d6f89
Divya <divyapallikonda259@gmail.com>
2025-04-09 17:49:05 +0530

    Update TransactionRepo.java

--- banking_transaction_servicemanagement/src/main/java/com/example/banking_transaction_servicemanagement/repo/TransactionRepo.java
+++ banking_transaction_servicemanagement/src/main/java/com/example/banking_transaction_servicemanagement/repo/TransactionRepo.java
@@ -5,5 +5,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.banking_transaction_servicemanagement.dto.Transaction;
 
 public interface TransactionRepo extends JpaRepository<Transaction, Integer> {
-  
+   List<Transaction> findByType(String type);
+
+	   
+	    List<Transaction> findByTimestampBetween(LocalDateTime start, LocalDateTime end);
+	    @Query("SELECT t FROM Transaction t WHERE " +
+	           "(:type IS NULL OR t.type = :type) AND " +
+	           "(:start IS NULL OR t.timestamp >= :start) AND " +
+	           "(:end IS NULL OR t.timestamp <= :end)")
+	    List<Transaction> findFiltered(
+	            @Param("type") String type,
+	            @Param("start") LocalDateTime start,
+	            @Param("end") LocalDateTime end);
+	   
+}
 }
